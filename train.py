24eedb25c68b5945887c11121bde4141048cfb50
John.Varghese <167159917+johnkallimelvarghese@users.noreply.github.com>
2026-03-24 00:36:50 +0530

    Increase learning rate from 0.001 to 0.01

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ model.train(
     data=r"/Users/john.jr/Documents/TCS/git repo1/dataset/dataset.yaml",
     epochs=75,
     imgsz=640,
-    lr0=0.001,
+    lr0=0.01,
     save_period=5,
     device="mps",
     workers=0,
